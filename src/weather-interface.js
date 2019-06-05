46d3ece4f6c6063aa7d8a66b01276816d8df5f75
kerriann walker and lindsey baker <kerriann@>
2019-06-05 09:51:28 -0700

    Refactor callback from jquery to javascript

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -1,19 +1,24 @@
-$(document).ready(function(){
-    $('#weatherLocation').click(function(){
-        let city = $('#location').val();
-        $.ajax({
-            url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6a33b6271afb4ed28ea9f07c3f231a1c`,
-            type: 'GET',
-            data: {
-                format: 'json'
-            },
-            success: function(response) {
-                $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-                $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-            },
-            error: function() {
-                $('#errors').text("There was an error processing your request. Please try again.");
+$(document).ready(function () {
+    $('#weatherLocation').click(function () {
+        const city = $('#location').val();
+        $('#location').val("");
+
+        let request = new XMLHttpRequest();
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6a33b6271afb4ed28ea9f07c3f231a1c`;
+
+        request.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                const response = JSON.parse(this.responseText);
+                getElements(response);
             }
-        });
+        }
+
+        request.open("GET", url, true);
+        request.send();
+
+        const getElements = function (response) {
+            $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+            $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+        }
     });
 });
